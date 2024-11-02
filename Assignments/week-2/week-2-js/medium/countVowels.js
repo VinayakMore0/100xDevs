0820e2fafd8d290742d5c03601e331a0307ca95d
Vinayak More <vinayakmore4052@gmail.com>
2024-11-02 20:28:54 +0530

    w2assMid3 done

--- Assignments/week-2/week-2-js/medium/countVowels.js
+++ Assignments/week-2/week-2-js/medium/countVowels.js
@@ -6,7 +6,7 @@
 */
 
 function countVowels(str) {
-    const vowels = [a, e, i, o, u];
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     let count = 0;
     for(let i = 0; i < str.length; i++){
         if(vowels.includes(str[i].toLowerCase())){
